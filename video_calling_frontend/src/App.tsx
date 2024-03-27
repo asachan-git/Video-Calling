aba00ffd3f2b14f5ee3e039c986f83d0beaf6e48
Aniket Sachan <aniket@rigi.club>
2024-03-27 12:47:19 +0530

    fixed untracked files

---
+++ video_calling_frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h5>Video calling App</h5>
+      <button>Click to start a meeting</button>
+    </div>
+  )
+}
+
+export default App
